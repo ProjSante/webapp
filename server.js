cb7c576e3bcfc9c3d72b931697d4725039fa81dd
Kmelow <camilo.c@protonmail.com>
2019-10-21 14:11:35 +0200

    Hotfix refactor

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ const app = express();
 app.use(express.json());
 
 // Routes
-/** For testing purposes */
-app.get('/', (req, res) => res.send('API working').status(200));
-/** Main API routes */
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/patients', require('./routes/api/patients'));
