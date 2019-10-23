ae5a4ed153240efb008bc21449bcde4ab2fc3c8a
Kmelow <camilo.c@protonmail.com>
2019-10-23 10:30:24 +0200

    :recycle: Refactor alert action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,33 +1,20 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import uuid from 'uuid';
 
-export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
-	const id = uuid.v4();
+export const setAlert = (msg, alertType, id, timeout = 5000) => (dispatch) => {
+	if (id === undefined) id = uuid.v4();
+
 	dispatch({
 		type    : SET_ALERT,
 		payload : { msg, alertType, id }
 	});
 
-	setTimeout(
-		() =>
-			dispatch({
-				type    : REMOVE_ALERT,
-				payload : id
-			}),
-		timeout
-	);
+	setTimeout(() => removeAlert(id), timeout);
 };
 
-export const toRemove = () => {
-	return {
-		type : SET_ALERT
-	};
-};
-
-// export const addTodo = () => ({ type: 'ADD_TODO' });
-export const addTodo = (msg) => {
-	return {
-		type : 'ADD_TODO',
-		msg
-	};
+export const removeAlert = (id) => (dispatch) => {
+	dispatch({
+		type    : REMOVE_ALERT,
+		payload : id
+	});
 };
