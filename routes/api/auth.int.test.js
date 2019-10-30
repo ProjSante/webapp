113155644551bb98feda942e751be1ee9acaff0a
Kmelow <camilo.c@protonmail.com>
2019-10-30 09:46:39 +0100

    Added basic integration test for /api/auth

---
+++ routes/api/auth.int.test.js
@@ -0,0 +1,98 @@
+// jest.mock('../../middleware/auth', () => jest.fn((req, res, next) => next()));
+
+// jest.mock('../../models/User', () => {
+// 	return {
+// 		findById : jest.fn((_id) => this),
+// 		select   : jest.fn(([ a, b ]) => this)
+// 	};
+// });
+
+const request = require('supertest');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/', require('./auth'));
+
+describe('[Integration] => API api/auth', () => {
+	describe('POST api/auth', () => {
+		test('Fail without email and password formData', async () => {
+			const response = await request(app).post('/');
+			expect(response.text).toEqual(
+				JSON.stringify({
+					errors : [
+						{
+							msg      : 'Please include valid e-mail',
+							param    : 'email',
+							location : 'body'
+						},
+						{
+							msg      : 'Password required',
+							param    : 'password',
+							location : 'body'
+						}
+					]
+				})
+			);
+			expect(response.statusCode).toBe(400);
+		});
+
+		test('Fail without password formData', async () => {
+			const response = await request(app)
+				.post('/')
+				.type('application/json')
+				.send({ email: 'adam@gmail.com' });
+			expect(response.text).toEqual(
+				JSON.stringify({
+					errors : [
+						{
+							msg      : 'Password required',
+							param    : 'password',
+							location : 'body'
+						}
+					]
+				})
+			);
+			expect(response.statusCode).toBe(400);
+		});
+
+		test('Fail without email formData', async () => {
+			const response = await request(app)
+				.post('/')
+				.type('application/json')
+				.send({ password: 'adam123!' });
+			expect(response.text).toEqual(
+				JSON.stringify({
+					errors : [
+						{
+							msg      : 'Please include valid e-mail',
+							param    : 'email',
+							location : 'body'
+						}
+					]
+				})
+			);
+			expect(response.statusCode).toBe(400);
+		});
+	});
+
+	describe('GET api/auth', () => {
+		test('Fail with non valid auth token', async () => {
+			const response = await request(app).get('/').set({
+				'content-type' : 'application/json',
+				'x-auth-token' : 'token.not.valid'
+			});
+			expect(response.text).toEqual(
+				JSON.stringify({
+					errors : [
+						{
+							msg : 'Token not valid'
+						}
+					]
+				})
+			);
+			expect(response.statusCode).toBe(401);
+		});
+	});
+});
