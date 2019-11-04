50c656757ef6b4027dfbed50ebdce7cffc1ba85f
Kmelow <camilo.c@protonmail.com>
2019-11-04 12:34:23 +0100

    :white_check_mark: Adding unit test for relays

--- __tests__/unit/relays.unit.test.js
+++ __tests__/unit/relays.unit.test.js
@@ -1,11 +1,33 @@
-jest.mock('../../middleware/auth', () => jest.fn((req, res, next) => next()));
-
 const relays = require('../../controllers/relays');
+const httpMocks = require('node-mocks-http');
+const User = require('../../models/User');
+const user = require('../mocks/user.json');
+
+User.findById = jest.fn((id) => {
+	if (id === user._id) {
+		return user;
+	} else {
+		throw new Error('invalid token');
+	}
+});
+
+let req, res;
+beforeEach(() => {
+	req = httpMocks.createRequest();
+	res = httpMocks.createResponse();
+	req.user = { id: '5db977aa4' };
+});
 
 describe('[Unit] => Controllers relays', () => {
 	describe('relays.addMeasurement', () => {
 		it('Contains addMeasurement function', (done) => {
 			expect(typeof relays.addMeasurement).toBe('function');
+			done();
+		});
+
+		it('Calls User.findById()', async (done) => {
+			await relays.addMeasurement(req, res);
+			expect(User.findById).toBeCalledWith(req.user.id);
 			done();
 		});
 	});
@@ -15,11 +37,43 @@ describe('[Unit] => Controllers relays', () => {
 			expect(typeof relays.getMeasurements).toBe('function');
 			done();
 		});
+
+		it('Calls User.findById()', async (done) => {
+			await relays.getMeasurements(req, res);
+			expect(User.findById).toBeCalledWith(req.user.id);
+			expect(res.statusCode).toBe(200);
+			done();
+		});
+
+		it('Fails when calling inexistent user', async (done) => {
+			// Bad user id
+			req.user = { id: '101010101' };
+			await relays.getMeasurements(req, res);
+			expect(User.findById).toBeCalledWith(req.user.id);
+			expect(res.statusCode).toBe(500);
+			done();
+		});
 	});
 
 	describe('relays.getDevices', () => {
 		it('Contains getDevices function', (done) => {
 			expect(typeof relays.getDevices).toBe('function');
+			done();
+		});
+
+		it('Calls User.findById()', async (done) => {
+			await relays.getDevices(req, res);
+			expect(User.findById).toBeCalledWith(req.user.id);
+			expect(res.statusCode).toBe(200);
+			done();
+		});
+
+		it('Fails when calling inexistent user', async (done) => {
+			// Bad user id
+			req.user = { id: '101010101' };
+			await relays.getDevices(req, res);
+			expect(User.findById).toBeCalledWith(req.user.id);
+			expect(res.statusCode).toBe(500);
 			done();
 		});
 	});
